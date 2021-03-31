241d191e3c4ae03ef7040705880eaa1808ebb1d2
Lagrang3 <eduardo.quintana@pm.me>
2021-03-31 16:25:56 +0200

    ex01: root of unity explicitely declared 'w'

--- examples/ex01.cpp
+++ examples/ex01.cpp
@@ -22,15 +22,15 @@ int main()
     const cd I2Pi = cd{0.0, 1.0} * 2.0 * std::acos(-1.0);
 
     std::vector<cd> A{1.0, 2.0, 3.0, 4.0};
+    const cd w = std::exp(I2Pi * (-1.0 / A.size()));
     print(A);
 
     // perform DFT on A
-    auto FT_A = fftx::FFT_Iterative(A, std::exp(I2Pi * (-1.0 / A.size())));
+    auto FT_A = fftx::FFT_Iterative(A, w);
     print(FT_A);
 
     // DFT on FT_A with inverse root of unity (aka Inverse DFT)
-    auto FT_FT_A =
-        fftx::FFT_Iterative(FT_A, std::exp(I2Pi * (1.0 / FT_A.size())));
+    auto FT_FT_A = fftx::FFT_Iterative(FT_A, 1.0 / w);
     print(FT_FT_A);
 
     auto diff = std::transform_reduce(
